1ae184828c045ccf38f37c0ff88badc6864353cb
Ronald Abu Saleh <lancerrnld@gmail.com>
2021-12-25 18:22:04 -0400

    Eslint and Prettier added

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,24 +1,25 @@
-// prettier-ignore
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // extends: ['eslint:recommended', 'plugin:react/jsx-runtime'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/jsx-runtime"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2021,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // 'react/react-in-jsx-scope': 'off',
+  },
 };
